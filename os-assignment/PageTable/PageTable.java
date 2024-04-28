0097bd863059b03ab76b683b3900e2bce64b5210
sceneryadmirer <nuraynurullayeva@gmail.com>
2024-04-28 13:06:41 +0400

    A few changes have been made

--- os-assignment/PageTable/PageTable.java
+++ os-assignment/PageTable/PageTable.java
@@ -1,24 +1,26 @@
 package PageTable;
 
+import util.Constants;
+
 public class PageTable {
-    private final int[] pageTable = new int[256];
+    private final int[] pageTable = new int[Constants.RAM_SIZE];
 
     public PageTable() {
 
-        for (int i = 0; i < 256; i++) {
+        for (int i = 0; i < Constants.RAM_SIZE; i++) {
             pageTable[i] = -1;
         }
     }
 
     public boolean doesFrameExist(int pageNumber) {
-        if (pageNumber < 0 || pageNumber > 255) {
+        if (pageNumber < 0 || pageNumber > Constants.PAGE_SIZE) {
             return false;
         }
         return pageTable[pageNumber] != -1;
     }
 
     public int getFrameNumber(int pageNumber) {
-        if (pageNumber < 0 || pageNumber > 255) {
+        if (pageNumber < 0 || pageNumber > Constants.PAGE_SIZE) {
             return -1000000;
         }
         if (pageTable[pageNumber] == -1) {
@@ -28,7 +30,7 @@ public class PageTable {
     }
 
     public void setFrameNumber(int pageNumber, int frameNumber) {
-        if (pageNumber < 0 || pageNumber > 255) {
+        if (pageNumber < 0 || pageNumber > Constants.PAGE_SIZE) {
             return;
         }
         pageTable[pageNumber] = frameNumber;
